175080d96d5ebf868484016ef54ce5829cf2d9e5
João Marcos <jottasintonia@gmail.com>
2022-06-30 23:39:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,4 +16,10 @@ function tocaSomTim () {
 
 document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff
+
 
