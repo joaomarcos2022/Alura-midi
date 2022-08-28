31a3f9ac86d28f693ee3d5f07c2ed9aafdd82b01
joaomarcos2022 <jottasintonia@gmail.com>
2022-08-28 14:42:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,6 @@
+// 
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play();
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-
-function tocaSomPuff () {
-    document.querySelector('#som_tecla_puff').play();
-}
-
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-
+document.querySelectorAll('.tecla')
