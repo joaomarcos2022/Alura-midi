f6417680e456c0a426f25b54a158db879947af7a
joaomarcos2022 <jottasintonia@gmail.com>
2022-08-28 16:51:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,7 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
