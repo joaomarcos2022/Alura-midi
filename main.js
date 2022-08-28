6dbf98a8cdc327ffdf44a00408c91f601fce216f
joaomarcos2022 <jottasintonia@gmail.com>
2022-08-28 17:50:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-// 
- function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (var i = 0; i < listaDeTeclas.length; i++) {
 
-while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
 
-    console.log(contador);
-
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
 
