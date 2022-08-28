ede975b415f543d814db888f92ab444690d5fe0c
joaomarcos2022 <jottasintonia@gmail.com>
2022-08-28 17:25:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 // 
-function tocaSomPom () {
+ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
 
