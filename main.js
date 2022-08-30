354687976a726175ed4e84a7d1ea55fbb31fca5f
joaomarcos2022 <jottasintonia@gmail.com>
2022-08-29 23:22:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,4 +15,8 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function () {
+        tecla.classList.add('')
+    }
+
 }
